44ecd2c019a4f960df0474a90b92132aaa955f7d
Yossie21 <janis.jaunzems2005@gmail.com>
2021-12-18 19:39:26 +0200

    Spēles sākšanas funkcijas inicializācija

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var v = [["Roberts L.", "Vēlu Jums priecīgus un baltus Ziemassvētkus!"],
         ["Markuss Z.","Lai ir pacietības visu dienu čakarēties ar skolas datoriem un skolēniem."],
         ["Jānis Helvijs J.","Vēlos pateikt paldies par šiem 4 mēnešiem prieka, tuvības, labvēlības un citām izcilām iezīmēm, kuras Jums piemīt! Paldies Jums liels!"],
-        ["Maksis G.","Sievietes ir kā vīns - jo vecāks, jo labāks. Novēlu kļūt vecākai un nebaidīties no tā!"],
+        ["Maksis Kārlis G.","Sievietes ir kā vīns - jo vecāks, jo labāks. Novēlu kļūt vecākai un nebaidīties no tā!"],
         ["Ernests K.", "Priecīgus Ziemassvētkus un, lai jaunais gads, atnes prieku un laimi."],
         ["Veronika L.", "Jaukus Ziemassvētkus un lai visas programmas strādātu no pirmas reizes!"],
         ["Kārlis Z.", "Lai viss izdodas!"]];
@@ -29,3 +29,8 @@ function moveBack(){
     }
     UpdateText();
 };
+
+function startGame()
+{
+    document.getElementById("Name").innerHTML = "Spēle is sākusies";
+}
